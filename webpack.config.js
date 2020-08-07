e8c0409331602a4aee3fd31c1545b755334c970d
Bailey Herbert <hello@bailey.sh>
2020-08-07 15:17:25 -0700

    Clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,8 @@ const sveltePath = path.resolve('node_modules', 'svelte');
 
 
 /**
- * Should source maps be generated alongside your production bundle? This will expose your raw source
- * code, so it's disabled by default.
+ * Should source maps be generated alongside your production bundle? This will expose your raw source code, so it's
+ * disabled by default.
  */
 const sourceMapsInProduction = false;
 
@@ -35,173 +35,173 @@ const useBabelInDevelopment = false;
  * styles will always appear last in the bundle.
  */
 const stylesheets = [
-	'./src/styles/index.scss'
+    './src/styles/index.scss'
 ];
 
 
 module.exports = {
-	entry: {
-		bundle: [
-			'./src/main.ts'
-		]
-	},
-	resolve: {
-		alias: {
-			svelte: path.resolve('node_modules', 'svelte')
-		},
-		extensions: ['.mjs', '.js', '.ts', '.svelte'],
-		mainFields: ['svelte', 'browser', 'module', 'main']
-	},
-	output: {
-		publicPath: '/build/',
-		path: __dirname + '/public/build',
-		filename: '[name].js',
-		chunkFilename: '[name].[id].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svelte$/,
-				use: {
-					loader: 'svelte-loader-hot',
-					options: {
-						dev: !prod,
-						emitCss: prod,
-						hotReload: !prod,
-						hotOptions: {
-							// List of options and defaults: https://www.npmjs.com/package/svelte-loader-hot#usage
-							noPreserveState: false,
-							optimistic: true
-						},
-						preprocess: Preprocess({
-							scss: true,
-							postcss: {
-								plugins: [
-									require('autoprefixer')
-								]
-							}
-						})
-					}
-				}
-			},
-			{
-				test: /\.s?css$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							hmr: !prod,
-							sourceMap: !prod || sourceMapsInProduction
-						}
-					},
-					'css-loader',
-					'sass-loader',
-					{
-						loader: 'postcss-loader',
-						options: {
-							plugins: [
-								require('autoprefixer')
-							]
-						}
-					}
-				]
-			},
-			{
-				test: /\.ts$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			}
-		]
-	},
-	devServer: {
-		hot: true,
-		stats: 'minimal',
-		contentBase: 'public',
-		watchContentBase: true
-	},
-	mode,
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		})
-	],
-	optimization: {
-		minimizer: []
-	},
-	devtool: (prod && !sourceMapsInProduction) ? false: 'source-map'
+    entry: {
+        bundle: [
+            './src/main.ts'
+        ]
+    },
+    resolve: {
+        alias: {
+            svelte: path.resolve('node_modules', 'svelte')
+        },
+        extensions: ['.mjs', '.js', '.ts', '.svelte'],
+        mainFields: ['svelte', 'browser', 'module', 'main']
+    },
+    output: {
+        publicPath: '/build/',
+        path: __dirname + '/public/build',
+        filename: '[name].js',
+        chunkFilename: '[name].[id].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svelte$/,
+                use: {
+                    loader: 'svelte-loader-hot',
+                    options: {
+                        dev: !prod,
+                        emitCss: prod,
+                        hotReload: !prod,
+                        hotOptions: {
+                            // List of options and defaults: https://www.npmjs.com/package/svelte-loader-hot#usage
+                            noPreserveState: false,
+                            optimistic: true
+                        },
+                        preprocess: Preprocess({
+                            scss: true,
+                            postcss: {
+                                plugins: [
+                                    require('autoprefixer')
+                                ]
+                            }
+                        })
+                    }
+                }
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: !prod,
+                            sourceMap: !prod || sourceMapsInProduction
+                        }
+                    },
+                    'css-loader',
+                    'sass-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                require('autoprefixer')
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        hot: true,
+        stats: 'minimal',
+        contentBase: 'public',
+        watchContentBase: true
+    },
+    mode,
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    optimization: {
+        minimizer: []
+    },
+    devtool: (prod && !sourceMapsInProduction) ? false: 'source-map'
 };
 
 // Add stylesheets to the build
 if (Array.isArray(stylesheets) || typeof stylesheets === 'string') {
-	if (!Array.isArray(stylesheets)) {
-		stylesheets = [stylesheets];
-	}
+    if (!Array.isArray(stylesheets)) {
+        stylesheets = [stylesheets];
+    }
 
-	module.exports.entry.bundle.unshift.apply(
-		module.exports.entry.bundle,
-		stylesheets
-	);
+    module.exports.entry.bundle.unshift.apply(
+        module.exports.entry.bundle,
+        stylesheets
+    );
 }
 
 // Load path mapping from tsconfig
 const tsconfigPath = path.resolve(__dirname, 'tsconfig.json');
 const tsconfig = require('fs').existsSync(tsconfigPath) ? require(tsconfigPath) : {};
 if ('compilerOptions' in tsconfig && 'paths' in tsconfig.compilerOptions) {
-	const aliases = tsconfig.compilerOptions.paths;
-	for (const alias in aliases) {
-		const paths = aliases[alias].map(p => path.resolve(__dirname, p));
+    const aliases = tsconfig.compilerOptions.paths;
+    for (const alias in aliases) {
+        const paths = aliases[alias].map(p => path.resolve(__dirname, p));
 
-		if (!(alias in module.exports.resolve.alias) && paths.length) {
-			module.exports.resolve.alias[alias] = paths.length > 1 ? paths : paths[0];
-		}
-	}
+        if (!(alias in module.exports.resolve.alias) && paths.length) {
+            module.exports.resolve.alias[alias] = paths.length > 1 ? paths : paths[0];
+        }
+    }
 }
 
 // These options should only apply to production builds
 if (prod) {
-	// Clean the build directory for production builds
-	module.exports.plugins.push(new CleanWebpackPlugin());
+    // Clean the build directory for production builds
+    module.exports.plugins.push(new CleanWebpackPlugin());
 
-	// Minify CSS
-	module.exports.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({
-		cssProcessorOptions: {
-			map: sourceMapsInProduction ? {
-				inline: false,
-				annotation: true
-			} : false,
-		},
-	}));
+    // Minify CSS
+    module.exports.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: {
+            map: sourceMapsInProduction ? {
+                inline: false,
+                annotation: true
+            } : false,
+        },
+    }));
 
-	// Minify and treeshake JS
-	module.exports.optimization.minimizer.push(new TerserPlugin({
-		sourceMap: sourceMapsInProduction
-	}));
+    // Minify and treeshake JS
+    module.exports.optimization.minimizer.push(new TerserPlugin({
+        sourceMap: sourceMapsInProduction
+    }));
 }
 
 // Add babel if enabled
 if (useBabel && (prod || useBabelInDevelopment)) {
-	module.exports.module.rules.unshift({
-		test: /\.(?:svelte|m?js)$/,
+    module.exports.module.rules.unshift({
+        test: /\.(?:svelte|m?js)$/,
 
-		// Svelte internals, under node_modules MUST be included.
-		//
-		// Babel 7 ignores node_modules automatically, but not if they're
-		// explicitely included.
-		// see: https://github.com/babel/babel-loader/issues/171#issuecomment-486380160
-		//
-		include: [
-			path.resolve(__dirname, 'src'),
-			path.dirname(sveltePath)
-		],
-		use: {
-			loader: 'babel-loader',
-			options: {
-				presets: [
-					[
-						'@babel/preset-env'
-					],
-				],
-			},
-		},
-	});
+        // Svelte internals, under node_modules MUST be included.
+        //
+        // Babel 7 ignores node_modules automatically, but not if they're
+        // explicitely included.
+        // see: https://github.com/babel/babel-loader/issues/171#issuecomment-486380160
+        //
+        include: [
+            path.resolve(__dirname, 'src'),
+            path.dirname(sveltePath)
+        ],
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    [
+                        '@babel/preset-env'
+                    ],
+                ],
+            },
+        },
+    });
 }
